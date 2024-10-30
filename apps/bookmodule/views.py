587cd93e7cae8c3c84ef66cadcbc5b776a214f2b
shoch77 <shrooqalqaied@gmail.com>
2024-10-30 15:27:04 +0300

    Initial commit of Django project

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+ return render(request, "bookmodule/index.html")
+
+def index2(request, val1=0):
+   return HttpResponse("value1 = " + str(val1))
+
+def list_books(request):
+ return render(request, 'bookmodule/list_books.html')
+
+def viewbook(request, bookId):
+ return render(request, 'bookmodule/one_book.html')
+
+def aboutus(request):
+ return render(request, 'bookmodule/aboutus.html')
+
+
+def viewbook(request, bookId):
+   
+   book1 = {'id':123, 'title':'Continuous Delivery', 'author':'J. Humble and D. Farley'}
+   book2 = {'id':456, 'title':'Secrets of Reverse Engineering', 'author':'E. Eilam'}
+   
+   argetBook = None
+   if book1['id'] == bookId: targetBook = book1
+   if book2['id'] == bookId: targetBook = book2
+   
+   context = {'book':targetBook} # book is the variable name accessible by the template
+   
+   return render(request, 'bookmodule/show.html', context)
--- libraryproject/urls.py
+++ libraryproject/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+import apps.bookmodule.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', apps.bookmodule.views.index, name='home'),  # Home page
+    path('books/', include("apps.bookmodule.urls")),  # Include the URLs for bookmodule
+    path('users/', include("apps.usermodule.urls")),    # Include the URLs for usermodule
 ]
