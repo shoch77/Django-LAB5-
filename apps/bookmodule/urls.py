2f4c467e3236b812cebd6c42f4c74bd174c504ee
shoch77 <shrooqalqaied@gmail.com>
2024-11-03 14:38:04 +0300

    lab 5 modified -->
    	modified:   apps/bookmodule/urls.py
    	modified:   apps/bookmodule/views.py
    	new file:   apps/static/desert.jpg
    	new file:   apps/static/styleMain.css
    	modified:   apps/static/styles.css
    	modified:   apps/templates/bookmodule/aboutus.html
    	new file:   apps/templates/bookmodule/formatting.html
    	modified:   apps/templates/bookmodule/includes/header.html
    	modified:   apps/templates/bookmodule/index.html
    	modified:   apps/templates/bookmodule/layouts/base.html
    	new file:   apps/templates/bookmodule/links.html
    	modified:   apps/templates/bookmodule/list_books.html
    	new file:   apps/templates/bookmodule/listing.html
    	modified:   apps/templates/bookmodule/one_book.html
    	deleted:    apps/templates/bookmodule/show.html
    	new file:   apps/templates/bookmodule/tables.html

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
     path('', views.index, name= "books.index"),
     path('list_books/', views.list_books, name= "books.list_books"),
-    path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
-    path('book/<int:bookId>/', views.viewbook, name="books.show_book"),  
+    path('<int:bookId>/', views.viewbook, name="books.one_book"),
     path('aboutus/', views.aboutus, name="books.aboutus"),
+
+    path('html5/links', views.links_page, name='links'),
+    path('html5/text/formatting', views.formatting_page, name='formatting'),
+    path('html5/listing', views.listing_page, name='listing'),
+    path('html5/tables', views.tables_page, name='tables'),
 ]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -12,21 +12,32 @@ def list_books(request):
  return render(request, 'bookmodule/list_books.html')
 
 def viewbook(request, bookId):
- return render(request, 'bookmodule/one_book.html')
+    
+    book1 = {'id': 123, 'title': 'Continuous Delivery', 'author': 'J. Humble and D. Farley'}
+    book2 = {'id': 456, 'title': 'Secrets of Reverse Engineering', 'author': 'E. Eilam'}
+
+    # Find the target book
+    targetBook = None
+    if book1['id'] == bookId:
+        targetBook = book1
+    elif book2['id'] == bookId:
+        targetBook = book2
+
+    context = {'book': targetBook}  # 'book' is the variable name accessible by the template
+
+    return render(request, 'bookmodule/one_book.html', context)
 
 def aboutus(request):
  return render(request, 'bookmodule/aboutus.html')
 
+def links_page(request):
+  return render(request, 'bookmodule/links.html') 
 
-def viewbook(request, bookId):
-   
-   book1 = {'id':123, 'title':'Continuous Delivery', 'author':'J. Humble and D. Farley'}
-   book2 = {'id':456, 'title':'Secrets of Reverse Engineering', 'author':'E. Eilam'}
-   
-   argetBook = None
-   if book1['id'] == bookId: targetBook = book1
-   if book2['id'] == bookId: targetBook = book2
-   
-   context = {'book':targetBook} # book is the variable name accessible by the template
-   
-   return render(request, 'bookmodule/show.html', context)
+def formatting_page(request):
+  return render(request, 'bookmodule/formatting.html')  
+
+def listing_page(request):
+  return render(request, 'bookmodule/listing.html') 
+
+def tables_page(request):
+  return render(request, 'bookmodule/tables.html') 
